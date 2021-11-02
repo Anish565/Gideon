23ba3ceb0af08d4aac91e2f4ae5ab7005586a268
Sanan <mohammedsanan18@gmail.com>
2021-11-02 21:33:26 +0530

    some work done finally

--- Main.py
+++ Main.py
@@ -5,6 +5,7 @@ import speech_recognition as s
 import datetime,random
 import pyttsx3
 import wikipedia
+import whatsapp as kit
 
 engine = pyttsx3.init('sapi5')
 
@@ -15,30 +16,30 @@ engine.setProperty('voice',voice[1].id)
 engine.setProperty('rate',150)
 
 
-def output(audio):
+def speak(audio):
     engine.say(audio)
     engine.runAndWait()
 
 
-def input():
+def listen():
     r = s.Recognizer()
     with s.Microphone() as source:
-        output("I'm Listening")
+        speak("I'm Listening")
         print("Listening...")
         r.pause_threshold = 1
         r.energy_threshold = 100
         audio = r.listen(source)
 
     try:
-        # output("hmm")
-        query = r.recognize_google(audio, language='en-us')
+        # speak("hmm")
+        query = r.recognize_google(audio, language='en-in')
         print(query)
     except Exception as e:
         i = random.randint(1,2)
         if i == 1:
-            output("I am sorry. I didn't get that")
+            speak("I am sorry. I didn't get that")
         elif i == 2:
-            output("Could you repeat that please.")
+            speak("Could you repeat that please.")
         return "None"
     return query        
 
@@ -52,33 +53,37 @@ def sendEmail(to, content):
     server.sendmail("email@gmail.com",to,content)
     server.close()
 
+# def sendwhats(m,n,h,mi):
+    
+#     kit.sendwhatmsg(n,m,h,mi)
+
 def wishMe():
     time = int(datetime.datetime.now().hour)
     if time >= 0 and time <12:
-        output("Good Morning")
+        speak("Good Morning")
     elif time >= 12 and time <4:
-        output("Good Afternoon")
+        speak("Good Afternoon")
     elif time>= 4 and time <= 7:
-        output("Good Evening")
+        speak("Good Evening")
     else:
-        output("Good Night")
+        speak("Good Night")
 
 
 
-# output("Hello There")
+# speak("Hello There")
 # wishMe()
 # while True:
 if 1:
-    query = input().lower()
+    query = listen().lower()
 
     if 'wikipedia' in query:
-        output("Searching Wikipedia")
+        speak("Searching Wikipedia")
         query = query.replace("wikipedia","")
         # print(query)
         results = wikipedia.summary(query, sentences=2)
-        output("So according to wikipedia")
-        output(results)
-        output("Hope that helped")
+        speak("So according to wikipedia")
+        speak(results)
+        speak("Hope that helped")
 
     elif 'open youtube' in query:
         query = query.replace("open youtube","")
@@ -102,18 +107,37 @@ if 1:
         os.startfile(os.path.join(music_dir,songs[0]))
     elif 'the time' in query:
         nowTime = datetime.datetime.now().strftime("%H:%M%p")
-        output(f"The time is {nowTime}")
+        speak(f"The time is {nowTime}")
     elif 'open code' in query:
         codepath = "C:\\Users\\Anisn\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Visual Studio Code\\Visual Studio Code.lnk"
         os.startfile(codepath)
     elif 'send email' in query:
         try:
-            output("What should i send?")
-            content = input()
+            speak("What should i send?")
+            content = listen()
             to = "anishnimbalkar@gmail.com"
             sendEmail(to, content)
-            output("Email has been sent")
+            speak("Email has been sent")
         except Exception as e:
             print(e)
-            output("There has been an error.")
+            speak("There has been an error.")
+    elif 'whatsapp' in query:
+        try:
+            query=query.split(" ")
+            if 'to' in query:
+                receiver=query[query.index('to')+1]
+                speak(receiver)
+            else:
+                speak("Who do you want to send the message to?")
+                receiver=listen()
+            speak("What would you like to send to "+receiver)
+            m=listen()
+            print(m)
+            kit.sendwhatmsg("+919618152076",m,datetime.datetime.now().hour,datetime.datetime.now().minute+1)
+
+        except:
+            speak("I'm sorry, I didn't get you")
+
+
+
 
