eaddb007c5ad91fec0c051b0932d5c0ccd8badd3
Sanan <mohammedsanan18@gmail.com>
2021-11-19 17:40:00 +0530

    translate minor change

--- translate.py
+++ translate.py
@@ -3,7 +3,7 @@ from pprint import pprint
 
 def trans(t):
     translator=Translator()
-    translation = translator.translate("こんにちは私の男お元気ですか")
+    translation = translator.translate(t)
 # print(f"{translation.origin} ({translation.src}) --> {translation.text} ({translation.dest})")
 # print(translation.text)
     return translation.text
